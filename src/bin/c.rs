fb04840e41f5bb015f196b81c1b01360b547cca6
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-14 00:37:32 +0900

    done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
+    }
+    let mut count_one = 0;
+    let mut count_four = 0;
+    for i in 0..n {
+        if a[i] % 4 == 0 {
+            count_four += 1;
+        } else if a[i] % 2 == 0 {
+        } else {
+            count_one += 1;
+        }
+    }
+    if count_four >= n / 2 {
+        println!("Yes");
+    } else if count_one > count_four {
+        println!("No");
+    } else {
+        println!("Yes");
     }
 }
